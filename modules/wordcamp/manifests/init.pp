4e9a43a8fbb4ee2cdea5c1712821ffba567de9c9
Stuart Shields <stuart.shields@xwp.co>
2017-02-06 08:00:24 +1000

    Create Modules

---
+++ modules/wordcamp/manifests/init.pp
@@ -0,0 +1,24 @@
+class wordcamp (
+	$path	= "/vagrant/extensions/wordcamp",
+) {
+	file { "${$path}/local-config.php":
+		content => template('wordcamp/local-config.php.erb'),
+		owner   => 'root',
+		group   => 'root',
+		mode    => 0644,
+	}
+
+	file { "/vagrant/content/mu-plugins":
+		ensure => "directory",
+		owner   => 'root',
+		group   => 'root',
+		mode    => 0644,
+	}
+
+	file { "/vagrant/content/mu-plugins/sandbox-functionality.php":
+		content => template('wordcamp/sandbox-functionality.php.erb'),
+		owner   => 'root',
+		group   => 'root',
+		mode    => 0644,
+	}
+}
---
+++ modules/wordcamp/manifests/plugins.pp
@@ -0,0 +1,44 @@
+class wordcamp::plugins (
+	$path	= "/vagrant/extensions/wordcamp",
+	$wordcamp_config = sz_load_config(),
+
+	$github_plugins = [
+		'svn2github/camptix-network-tools',
+		'svn2github/email-post-changes',
+		'svn2github/tagregator',
+		'Automattic/camptix',
+	],
+
+	$plugins = [
+		'akismet',
+		'buddypress',
+		'bbpress',
+		'camptix-pagseguro',
+		'camptix-payfast-gateway'
+		'jetpack',
+		'json-rest-api',
+		'wp-multibyte-patch',
+		'wordpress-importer',
+	]
+)
+{
+if ! ( File['/vagrant/content'] ) {
+		file { "/vagrant/content":
+			ensure => "directory",
+		}
+	}
+
+	if ! ( File['/vagrant/content/plugins'] ) {
+		file { "/vagrant/content/plugins":
+			ensure => "directory",
+		}
+	}
+
+	wordcamp::gitcheck { $github_plugins: }
+
+	# Install plugins
+	wp::plugin { $plugins:
+		location => '/vagrant/wp',
+		require  => Class['wp']
+	}
+}
