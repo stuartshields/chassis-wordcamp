22cd021df2bdf0f1d185e6366aeb3f755cfaf953
Stuart Shields <stuart.shields@xwp.co>
2017-02-10 20:46:08 +1000

    Update folder permissions

--- modules/wordcamp/manifests/init.pp
+++ modules/wordcamp/manifests/init.pp
@@ -3,43 +3,43 @@ class wordcamp (
 ) {
 	file { "${$path}/local-config.php":
 		content => template('wordcamp/local-config.php.erb'),
-		owner   => 'root',
-		group   => 'root',
+		owner   => 'www-data',
+		group   => 'www-data',
 		mode    => 0644,
 	}
 
 	file { "/vagrant/content/config":
 		ensure => "directory",
-		owner   => 'root',
-		group   => 'root',
+		owner   => 'www-data',
+		group   => 'www-data',
 		mode    => 0644,
 	}
 
 	file { "/vagrant/content/advanced-cache.php":
 		content => template('wordcamp/advanced-cache.php.erb'),
-		owner   => 'root',
-		group   => 'root',
+		owner   => 'www-data',
+		group   => 'www-data',
 		mode    => 0644,
 	}
 
 	file { "/vagrant/content/sunrise.php":
 		content => template('wordcamp/sunrise.php.erb'),
-		owner   => 'root',
-		group   => 'root',
+		owner   => 'www-data',
+		group   => 'www-data',
 		mode    => 0644,
 	}
 
 	file { "/vagrant/content/config/batcache-config.php":
 		content => template('wordcamp/batcache-config.php.erb'),
-		owner   => 'root',
-		group   => 'root',
+		owner   => 'www-data',
+		group   => 'www-data',
 		mode    => 0644,
 	}
 
 	file { "/vagrant/content/config/is-mobile.php":
 		content => template('wordcamp/is-mobile.php.erb'),
-		owner   => 'root',
-		group   => 'root',
+		owner   => 'www-data',
+		group   => 'www-data',
 		mode    => 0644,
 	}
 }
